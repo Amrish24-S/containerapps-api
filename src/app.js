11c9da6fa8386541e2724888e07fee3b33546f04
Amrish S <itzamrish@gmail.com>
2024-05-24 17:10:10 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -4,22 +4,6 @@ var logger = require("morgan");
 
 var router = require("./routes/index");
 var app = express();
-
-let appInsights = require("applicationinsights");
-appInsights.defaultClient.context.tags[appInsights.defaultClient.context.keys.cloudRole] = "api";
-appInsights.setup("InstrumentationKey=15bf10dc-4d16-4cd6-85ac-7de4018b0a78;IngestionEndpoint=https://canadacentral-1.in.applicationinsights.azure.com/;LiveEndpoint=https://canadacentral.livediagnostics.monitor.azure.com/;ApplicationId=a4d0504e-849f-42c2-af20-0d1f1fbcbad6")
-    .setAutoDependencyCorrelation(true)
-    .enableWebInstrumentation(true)
-    .setAutoCollectRequests(true)
-    .setAutoCollectPerformance(true, true)
-    .setAutoCollectExceptions(true)
-    .setAutoCollectDependencies(true)
-    .setAutoCollectConsole(true)
-    .setUseDiskRetryCaching(true)
-    .setSendLiveMetrics(true)
-    .setDistributedTracingMode(appInsights.DistributedTracingModes.AI_AND_W3C)
-    .start();
-
 //enable cors
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
